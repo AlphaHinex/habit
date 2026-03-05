57028b41dfbcc8ac5feca5cc9fae7448ff152bee
Alpha Hinex <AlphaHinex@gmail.com>
2026-03-05 21:20:28 +0800

    Could upload img to github

--- lark-ws/lark-ws.go
+++ lark-ws/lark-ws.go
@@ -2,14 +2,21 @@ package main
 
 import (
 	"context"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
+	lark "github.com/larksuite/oapi-sdk-go/v3"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
 	larkcore "github.com/larksuite/oapi-sdk-go/v3/core"
 	larkevent "github.com/larksuite/oapi-sdk-go/v3/event"
 	"github.com/larksuite/oapi-sdk-go/v3/event/dispatcher"
 	larkim "github.com/larksuite/oapi-sdk-go/v3/service/im/v1"
 	larkws "github.com/larksuite/oapi-sdk-go/v3/ws"
-	"os"
 )
 
 type TextMsg struct {
@@ -20,6 +27,104 @@ type ImgMsg struct {
 	ImageKey string `json:"image_key"`
 }
 
+type GitHubUploadRequest struct {
+	Message string `json:"message"`
+	Content string `json:"content"`
+	Branch  string `json:"branch"`
+}
+
+type GitHubUploadResponse struct {
+	Content struct {
+		Name string `json:"name"`
+		Path string `json:"path"`
+	} `json:"content"`
+}
+
+func getFileFromMsg(client *lark.Client, msgId, key, fileType string) ([]byte, error) {
+	// 创建请求对象
+	req := larkim.NewGetMessageResourceReqBuilder().
+		MessageId(msgId).
+		FileKey(key).
+		Type(fileType).
+		Build()
+
+	// 发起请求
+	resp, err := client.Im.V1.MessageResource.Get(context.Background(), req)
+
+	// 处理错误
+	if err != nil {
+		return nil, err
+	}
+
+	// 服务端错误处理
+	if !resp.Success() {
+		fmt.Printf("logId: %s, error response: \n%s", resp.RequestId(), larkcore.Prettify(resp.CodeError))
+		return nil, fmt.Errorf("failed to get image, code: %d, msg: %s", resp.Code, resp.Msg)
+	}
+
+	return resp.RawBody, nil
+}
+
+func uploadFileToGitHub(fileData []byte, fileName string) error {
+	// GitHub 仓库信息
+	repoOwner := "alphahinex"
+	repoName := "habit"
+	token := os.Getenv("GITHUB_TOKEN")
+	if token == "" {
+		return fmt.Errorf("GITHUB_TOKEN environment variable not set")
+	}
+
+	// 构建上传路径
+	uploadPath := fmt.Sprintf("fftq/res/%s/%s", time.Now().Format("20060102"), fileName)
+
+	// 构建 GitHub API URL
+	url := fmt.Sprintf("https://api.github.com/repos/%s/%s/contents/%s", repoOwner, repoName, uploadPath)
+
+	// 准备请求数据
+	content := base64.StdEncoding.EncodeToString(fileData)
+	reqBody := GitHubUploadRequest{
+		Message: fmt.Sprintf("Add %s", fileName),
+		Content: content,
+		Branch:  "master",
+	}
+
+	// 编码请求体
+	reqBodyJSON, err := json.Marshal(reqBody)
+	if err != nil {
+		return err
+	}
+
+	// 创建 HTTP 客户端
+	client := &http.Client{}
+
+	// 创建请求
+	req, err := http.NewRequest("PUT", url, nil)
+	if err != nil {
+		return err
+	}
+	req.Body = io.NopCloser(strings.NewReader(string(reqBodyJSON)))
+
+	// 添加认证头
+	req.Header.Add("Authorization", fmt.Sprintf("token %s", token))
+	req.Header.Add("Content-Type", "application/json")
+
+	// 发送请求
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// 检查响应状态码
+	if resp.StatusCode != http.StatusCreated && resp.StatusCode != http.StatusOK {
+		// 读取错误响应
+		errBody, _ := io.ReadAll(resp.Body)
+		return fmt.Errorf("failed to upload image, status code: %d, error: %s", resp.StatusCode, string(errBody))
+	}
+
+	return nil
+}
+
 func main() {
 	appID := os.Getenv("APP_ID")
 	appSecret := os.Getenv("APP_SECRET")
@@ -27,10 +132,14 @@ func main() {
 		panic("APP_ID or APP_SECRET environment variable not set")
 	}
 
+	// 创建 Client
+	client := lark.NewClient(appID, appSecret)
+
 	// 注册事件回调，OnP2MessageReceiveV1 为接收消息 v2.0；OnCustomizedEvent 内的 message 为接收消息 v1.0。
 	eventHandler := dispatcher.NewEventDispatcher("", "").
 		OnP2MessageReceiveV1(func(ctx context.Context, event *larkim.P2MessageReceiveV1) error {
 			fmt.Printf("[ OnP2MessageReceiveV1 access ], data: %s\n", larkcore.Prettify(event))
+			msgId := *event.Event.Message.MessageId
 			content := event.Event.Message.Content
 			var textMsg TextMsg
 			err := json.Unmarshal([]byte(*content), &textMsg)
@@ -41,6 +150,23 @@ func main() {
 				err = json.Unmarshal([]byte(*content), &imgMsg)
 				if err == nil && len(imgMsg.ImageKey) > 0 {
 					fmt.Printf("[ OnP2MessageReceiveV1 access ], image_key: %s\n", imgMsg.ImageKey)
+
+					// 获取图片
+					imageData, err := getFileFromMsg(client, msgId, imgMsg.ImageKey, "image")
+					if err != nil {
+						fmt.Printf("[ OnP2MessageReceiveV1 access ], failed to get image: %v\n", err)
+					} else {
+						// 生成文件名
+						fileName := fmt.Sprintf("%d.jpg", time.Now().Unix())
+
+						//上传到 GitHub
+						err = uploadFileToGitHub(imageData, fileName)
+						if err != nil {
+							fmt.Printf("[ OnP2MessageReceiveV1 access ], failed to upload image: %v\n", err)
+						} else {
+							fmt.Printf("[ OnP2MessageReceiveV1 access ], image uploaded successfully: %s\n", fileName)
+						}
+					}
 				}
 			}
 			return nil
@@ -49,6 +175,7 @@ func main() {
 			fmt.Printf("[ OnCustomizedEvent access ], type: message, data: %s\n", string(event.Body))
 			return nil
 		})
+
 	// 创建Client
 	cli := larkws.NewClient(appID, appSecret,
 		larkws.WithEventHandler(eventHandler),
